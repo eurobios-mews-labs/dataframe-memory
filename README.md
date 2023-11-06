47191a3f0a7f5c7088272410580182d7c8e62257
vlaurent <vlaurent@eurobios.com>
2023-11-06 17:47:39 +0100

    [DEV] update

--- README.md
+++ README.md
@@ -10,28 +10,38 @@ It is highly inspired from this [kaggle post](https://www.kaggle.com/gemartin/lo
 > - this tool reduces int and float precision without generating duplicates
 > - the data types are chosen so that the minimum and maximum values can be re-encoded
 
+### Usage
+
 ````python
 from data_memory import reduce_memory
 import numpy as np
 import pandas as pd
 
-df = pd.DataFrame(np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]] * 100),
-                  columns=['a', 'b', 'c'])
+df = pd.DataFrame(
+    np.array(
+        [[1, 2, "aaa"],
+         [4, 5, "bbb"],
+         [7, 8, "ccc"]] * 10000),
+    columns=['a', 'b', 'c'])
 
 reduce_memory(df, verbose=True)
-print(df.info())
 ````
+Yields the following decrease of memory
 ````text
-<class 'pandas.core.frame.DataFrame'>
-RangeIndex: 300 entries, 0 to 299
-Data columns (total 3 columns):
- #   Column  Non-Null Count  Dtype
----  ------  --------------  -----
- 0   a       300 non-null    uint8 <- best data type
- 1   b       300 non-null    uint8
- 2   c       300 non-null    uint8
-dtypes: uint8(3)
-memory usage: 1.0 KB
+Memory usage input: 5.04 MB
+Memory usage output: 0.09 MB
+Decreased by: 98.28 % 
+````
+````python
+df.info()
+````
+
+````text
+ #   Column  Non-Null Count  Dtype   
+---  ------  --------------  -----   
+ 0   a       30000 non-null  category
+ 1   b       30000 non-null  category
+ 2   c       30000 non-null  category
 ````
 
 > [!WARNING]
--- data_memory/main.py
+++ data_memory/main.py
@@ -15,69 +15,71 @@
 import numpy as np
 import pandas as pd
 
+INT_DATATYPE = ['uint8', 'uint16', 'uint32', 'uint64', 'int8', 'int16',
+                'int32', 'int64']
 
-def memory_usage(data_frame):
-    return data_frame.memory_usage(deep=True).sum() / 1024 ** 2
+FLOAT_DATATYPE = ['float16', 'float32', 'float64']
 
 
-def _reduce_float(data_frame, col):
-    subtype_float = ['float16', 'float32', 'float64']
-    mx_col = data_frame[col].max()
-    mn_col = data_frame[col].min()
-    n0 = data_frame[col].unique().__len__()
-    for ele in subtype_float:
-        c1 = mn_col > np.finfo(ele).min
-        c2 = mx_col < np.finfo(ele).max
-        n = data_frame[col].astype(ele).unique().__len__()
+def memory_usage(df: pd.DataFrame):
+    return df.memory_usage(deep=True).sum() / 1024 ** 2
+
+
+def _reduce_float(df: pd.DataFrame, column):
+    max_column = df[column].max()
+    min_column = df[column].min()
+    n0 = df[column].unique().__len__()
+    for dtype in FLOAT_DATATYPE:
+        c1 = min_column > np.finfo(dtype).min
+        c2 = max_column < np.finfo(dtype).max
+        n = df[column].astype(dtype).unique().__len__()
         c3 = n * 1.1 > n0
         if c1 and c2 and c3:
-            data_frame[col] = data_frame[col].astype(ele)
+            df[column] = df[column].astype(dtype)
             break
 
 
-def _reduce_int(data_frame, col):
-    subtype_int = ['uint8', 'uint16', 'uint32', 'uint64', 'int8', 'int16',
-                   'int32', 'int64']
-    mx_col = data_frame[col].max()
-    mn_col = data_frame[col].min()
-    n0 = data_frame[col].unique().__len__()
-    for ele in subtype_int:
-        c1 = mn_col > np.iinfo(ele).min
-        c2 = mx_col < np.iinfo(ele).max
-        n = data_frame[col].astype(ele).unique().__len__()
+def _reduce_int(df: pd.DataFrame, column):
+    max_column = df[column].max()
+    min_column = df[column].min()
+    n0 = df[column].unique().__len__()
+    for dtype in INT_DATATYPE:
+        c1 = min_column > np.iinfo(dtype).min
+        c2 = max_column < np.iinfo(dtype).max
+        n = df[column].astype(dtype).unique().__len__()
         c3 = n * 1.1 > n0
         if c1 and c2 and c3:
-            data_frame[col] = data_frame[col].astype(ele)
+            df[column] = df[column].astype(dtype)
             break
 
 
-def reduce_memory(data_frame, verbose=False, dates=None):
+def reduce_memory(df: pd.DataFrame, verbose=False, dates=None):
+    memory_before = 0
     if dates is None:
         dates = []
     if verbose:
-        memory_before = memory_usage(data_frame)
-        print('before: {:.2f} MB'.format(memory_before))
+        memory_before = memory_usage(df)
+        print('Memory usage input: {:.2f} MB'.format(memory_before))
+    n = len(df)
+    for column in df.columns:
+        column_type = str(df[column].dtypes)
 
-    for col in data_frame.columns:
-        col_type = str(data_frame[col].dtypes)
+        if 'int' in column_type:
+            _reduce_int(df, column)
 
-        if 'int' in col_type:
-            _reduce_int(data_frame, col)
+        elif 'float' in column_type:
+            _reduce_float(df, column)
 
-        elif 'float' in col_type:
-            _reduce_float(data_frame, col)
-
-        elif 'object' in col_type:
-            if col in dates:
-                data_frame[col] = pd.to_datetime(data_frame[col],
-                                                 format='%Y-%m-%d')
+        elif 'object' in column_type:
+            if column in dates:
+                df[column] = pd.to_datetime(df[column],
+                                            format='%Y-%m-%d')
             else:
-                numbr_of_unique = len(data_frame[col].unique())
-                numbr_total = len(data_frame[col])
-                if numbr_of_unique / numbr_total < 0.5:
-                    data_frame[col] = data_frame[col].astype('category')
+                nb_unique = len(df[column].unique())
+                if nb_unique / n < 0.5:
+                    df[column] = df[column].astype('category')
     if verbose:
-        memory_after = memory_usage(data_frame)
-        print('after:{:.2f} MB'.format(memory_after))
+        memory_after = memory_usage(df)
+        print('Memory usage output: {:.2f} MB'.format(memory_after))
         print('Decreased by: {:.2f} % '.format(
             100 * (memory_before - memory_after) / memory_before))
